5c548f004393304d27f84d14cd5168deef1b11d9
Roland Huß <roland@ro14nd.de>
2020-06-03 18:21:31 +0200

    release 0.15.1

--- kn@0.14.rb
+++ kn@0.14.rb
@@ -3,10 +3,10 @@ require "FileUtils"
 class Kn < Formula
   homepage "https://github.com/knative/client"
 
-  v = "v0.15.1"
+  v = "v0.14.0"
   url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
   version v
-  sha256 "b594731a04f49c6b130245193aaf93eb53235ca01abed9b5a97fb54ef8c18577"
+  sha256 "23ebdc0c675f13642ba54ee7d53350404eaa01a338e15741622b72f50ecce519"
 
   def install
     FileUtils.mv("kn-darwin-amd64", "kn")
