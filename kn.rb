eb73720d1f194b68a94efd513b7ef686ffc3a85e
feluxe <felix@egoversum.com>
2020-06-15 16:38:23 +0200

    Add linux support. (#7)
    * Add linux support.
    
    We are a mixed team of Mac/Linux users, therefor Linux support would be quite helpful for us. :)
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt Moore <mattmoor@vmware.com>
    
    Co-authored-by: Matt Moore <mattmoor@vmware.com>

--- kn.rb
+++ kn.rb
@@ -1,15 +1,29 @@
-require "FileUtils"
+if OS.mac?
+  require "FileUtils"
+else
+  require "fileutils"
+end
 
 class Kn < Formula
   homepage "https://github.com/knative/client"
 
   v = "v0.15.1"
-  url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
   version v
-  sha256 "b594731a04f49c6b130245193aaf93eb53235ca01abed9b5a97fb54ef8c18577"
+
+  if OS.mac?
+    url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
+    sha256 "b594731a04f49c6b130245193aaf93eb53235ca01abed9b5a97fb54ef8c18577"
+  else
+    url "https://github.com/knative/client/releases/download/#{v}/kn-linux-amd64"
+    sha256 "aa66d8f3c442219522bc8a135aa04b25700e2da9153b436c2d8259c6705ff88d"
+  end
 
   def install
-    FileUtils.mv("kn-darwin-amd64", "kn")
+    if OS.mac?
+      FileUtils.mv("kn-darwin-amd64", "kn")
+    else
+      FileUtils.mv("kn-linux-amd64", "kn")
+    end
     bin.install "kn"
   end
 
