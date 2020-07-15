e9879d0ae10d731ccdace88621c25524bf6750d1
Roland Huß <roland@ro14nd.de>
2020-07-15 11:04:54 +0200

    Update to kn 0.16.0 (#9)

--- kn@0.15.rb
+++ kn@0.15.rb
@@ -4,18 +4,18 @@ else
   require "fileutils"
 end
 
-class Kn < Formula
+class KnAT015 < Formula
   homepage "https://github.com/knative/client"
 
-  v = "v0.16.0"
+  v = "v0.15.2"
   version v
 
   if OS.mac?
     url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
-    sha256 "44fda335ff7ad133e4286b97d8760fd68c1719a758f9fc5c246e3da8a74893a1"
+    sha256 "c673b57a5fcc26b58b0491cff52d0de01e985d2cf87c6af5a9b730c16c7113c0"
   else
     url "https://github.com/knative/client/releases/download/#{v}/kn-linux-amd64"
-    sha256 "55de2e29384f72f3dc3468aae680a55f8f6b37472d0d03be97606863a5be362d"
+    sha256 "47b09a8b4ac9d738f0ab4ae1e78902745a49cd5721ce967d1f93b77c41f33b9e"
   end
 
   def install
