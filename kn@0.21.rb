96e71e6f108174f50ca2bd8511ecbd97debc7629
Navid Shaikh <nshaikh@redhat.com>
2021-04-07 12:39:49 +0530

    homebrew knative client release v0.22.0 (#26)

--- kn@0.21.rb
+++ kn@0.21.rb
@@ -1,17 +1,17 @@
 require "fileutils"
 
-class Kn < Formula
+class KnAT021 < Formula
   homepage "https://github.com/knative/client"
 
-  v = "v0.22.0"
+  v = "v0.21.0"
   version v
 
   if OS.mac?
     url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
-    sha256 "de91d4dc210ac6d3dadb3a5ffaebb2585cbb7241578596e675f1650dc09d6836"
+    sha256 "6daa5a9f1108b71bfcd22f293494f6b4bfd6d2937f76e8f7ba48e1d969e68d31"
   else
     url "https://github.com/knative/client/releases/download/#{v}/kn-linux-amd64"
-    sha256 "3f4131a616acc816a6548c3ae4bf5cf2ed41b36ffe9cb6c748a31b2b700ad009"
+    sha256 "f57427f523b2d882af796a87be47ffb061c2c0a0543b851af74f58ac2945f910"
   end
 
   def install
