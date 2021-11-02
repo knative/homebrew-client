22031ff6c4c89366ab472e512ab837b369d4fd46
David Simansky <dsimansk@redhat.com>
2021-11-02 20:54:46 +0100

    Update client to v1.0.0 (#36)

--- kn@0.26.rb
+++ kn@0.26.rb
@@ -1,17 +1,17 @@
 require "fileutils"
 
-class Kn < Formula
+class KnAT026 < Formula
   homepage "https://github.com/knative/client"
 
-  v = "knative-v1.0.0"
+  v = "v0.26.0"
   version v
 
   if OS.mac?
     url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
-    sha256 "7ca666b399b481fb6dd003535feeb8c9d3cf969ad50d481e9159b5770fba7844"
+    sha256 "cfd463f4fed51ac54359d0d3e40bf37275d0b781b7910fe88e7ac7c240fb261f"
   else
     url "https://github.com/knative/client/releases/download/#{v}/kn-linux-amd64"
-    sha256 "47fe4465e4d802a7d03ab01fe367af2ddda23b5eb4d87b575d83a9f32ba83a34"
+    sha256 "93248e1e2b665bfb68092cc7c49fd96405c9b008688a21d920aae9bc455b538f"
   end
 
   def install
