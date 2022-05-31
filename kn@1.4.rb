7a2fee5a9683b65be57f728990c82c9d1015ac6a
Gab Satchi <gsatchi@vmware.com>
2022-05-31 17:48:36 -0400

    update kn client to 1.5 (#56)

--- kn@1.4.rb
+++ kn@1.4.rb
@@ -1,17 +1,17 @@
 require "fileutils"
 
-class Kn < Formula
+class KnAT14 < Formula
   homepage "https://github.com/knative/client"
 
-  v = "knative-v1.5.0"
+  v = "knative-v1.4.0"
   version v
 
   if OS.mac?
     url "https://github.com/knative/client/releases/download/#{v}/kn-darwin-amd64"
-    sha256 "4319b9b2a7f0faa3db97acb42a942b2346b115b3bc27b2d950bdf8b7d91311e4"
+    sha256 "797e9996b82cbf92f95e132e69acb0a971b2b22cb5da11386db71d1987a09f29"
   else
     url "https://github.com/knative/client/releases/download/#{v}/kn-linux-amd64"
-    sha256 "663af0337985c7a1ea9b339cc0c865dba05bdacbcb8453fd63057e680c921f46"
+    sha256 "85f0d5f11186efbf3659f7d02e3c9d72e0dcc12057d681bbcea1709462bc5740"
   end
 
   def install
