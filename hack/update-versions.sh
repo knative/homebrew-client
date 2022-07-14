fbafdf3c10b7a19ce15f7c33660691fc0406103c
Paul Schweigert <paulschw@us.ibm.com>
2022-07-14 15:55:15 -0400

    Add script to update plugin versions (#63)
    * add script to update plugin versions
    
    Signed-off-by: Paul S. Schweigert <paulschw@us.ibm.com>
    
    * fixes
    
    Signed-off-by: Paul S. Schweigert <paulschw@us.ibm.com>
    
    * handle patch updates too
    
    Signed-off-by: Paul S. Schweigert <paulschw@us.ibm.com>

---
+++ hack/update-versions.sh
@@ -0,0 +1,86 @@
+#! /usr/bin/env bash
+
+# Copyright 2022 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+if [ -z "$CURRENT_RELEASE" ]; then
+    echo 'Please provide a value for $CURRENT_RELEASE'
+    exit 1
+fi
+
+if [ -z "$PREVIOUS_RELEASE" ]; then
+    echo 'Please provide a value for $PREVIOUS_RELEASE'
+    exit 1
+fi
+
+if [ ${CURRENT_RELEASE} = ${PREVIOUS_RELEASE} ]; then
+    echo 'Nothing to do.'
+    exit 0
+fi
+
+IFS='.'
+read -ra newversion <<< "$CURRENT_RELEASE"
+read -ra oldversion <<< "$PREVIOUS_RELEASE"
+unset IFS
+
+# validate
+if [ -z ${newversion[0]} ] || [ -z ${newversion[1]} ] || [ -z ${newversion[2]} ]; then
+    echo 'Please enter a valid value for $CURRENT_RELEASE in the form: x.y.z'
+    exit 1
+fi
+
+if [ -z ${oldversion[0]} ] || [ -z ${oldversion[1]} ] || [ -z ${oldversion[2]} ]; then
+    echo 'Please enter a valid value for $PREVIOUS_RELEASE in the form: x.y.z'
+    exit 1
+fi 
+
+NEW_SHORT="${newversion[0]}.${newversion[1]}"
+NEW_PATCH="${newversion[2]}"
+OLD_SHORT="${oldversion[0]}.${oldversion[1]}"
+OLD_PATCH="${oldversion[2]}"
+
+echo "Updating kn from $PREVIOUS_RELEASE to $CURRENT_RELEASE"
+newfile="kn.rb"
+oldfile="kn@${OLD_SHORT}.rb"
+
+# if patch release update, then we just need to update the existing kn.rb file
+if [ $NEW_SHORT != $OLD_SHORT ]; then
+    echo "Create formula for previous release"
+    cp "$newfile" "$oldfile"
+    class_version=${OLD_SHORT//.}
+    class_name="Kn"
+    sed -i -r "s/class\s+${class_name}\s+<\s+Formula/class ${class_name}AT${class_version} < Formula/" "$oldfile"
+    echo "$oldfile created"
+fi
+
+echo "Creating formula for current release"
+sed -i "s/v${PREVIOUS_RELEASE}/v${CURRENT_RELEASE}/g" "$newfile"
+
+# change shas for mac and linux
+checksums=$(mktemp)
+old_checksums=$(mktemp)
+curl -L "https://github.com/knative/client/releases/download/knative-v${CURRENT_RELEASE}/checksums.txt" > "$checksums"
+curl -L "https://github.com/knative/client/releases/download/knative-v${PREVIOUS_RELEASE}/checksums.txt" > "$old_checksums"
+darwin_checksum=$(awk '$2=="kn-darwin-amd64"{print $1}' "$checksums")
+darwin_old_checksum=$(awk '$2=="kn-darwin-amd64"{print $1}' "$old_checksums")
+sed -i 's/sha256 "'"$darwin_old_checksum"'"/sha256 "'"$darwin_checksum"'"/' "$newfile"
+linux_checksum=$(awk '$2=="kn-linux-amd64"{print $1}' "$checksums")
+linux_old_checksum=$(awk '$2=="kn-linux-amd64"{print $1}' "$old_checksums")
+sed -i 's/sha256 "'"$linux_old_checksum"'"/sha256 "'"$linux_checksum"'"/' "$newfile"
+
+echo "$newfile created"
+
+echo "Have a nice day!"
